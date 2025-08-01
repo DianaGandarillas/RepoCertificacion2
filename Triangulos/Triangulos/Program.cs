ceb66d3ad2e1521917f1bd5da8fdea6bf843543b
DianaGandarillas <dianagandarillas1@upb.edu>
2025-08-01 09:41:40 -0400

    primer commit con la practica1

---
+++ Triangulos/Triangulos/Program.cs
@@ -0,0 +1,33 @@
+﻿class Program
+{
+    static void Main()
+    {
+        // Solicitar los lados del triángulo
+        Console.Write("INGRESE EL LADO A DEL TRIANGULO: ");
+        double ladoA = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("INGRESE EL LADO B DEL TRIANGULO: ");
+        double ladoB = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("INGRESE EL LADO C DEL TRIANGULO: ");
+        double ladoC = Convert.ToDouble(Console.ReadLine());
+
+        // Determinar el tipo de triángulo
+        if (ladoA == ladoB && ladoB == ladoC)
+        {
+            Console.WriteLine("EL TRIANGULO ES EQUILATERO");
+        }
+        else if (ladoA == ladoB || ladoA == ladoC || ladoB == ladoC)
+        {
+            Console.WriteLine("EL TRIANGULO ES ISOSCELES");
+        }
+        else if (ladoA != ladoB || ladoA != ladoC || ladoB != ladoC)
+        {
+            Console.WriteLine("EL TRIANGULO ES ESCALENO");
+        }
+        else
+        {
+            Console.WriteLine("TRIANGULO NO ACEPTADO");
+        }
+    }
+}
